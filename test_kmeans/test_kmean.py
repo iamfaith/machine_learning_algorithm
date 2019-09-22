60b2c8c88d4be8a71ce215ca94c7e88cf89ae8c2
xianzixiang <xianzixiang@wps.cn>
2019-09-22 18:53:03 -0400

    add k-means ++ algorithm

--- test_kmeans/test_kmean.py
+++ test_kmeans/test_kmean.py
@@ -1,3 +1,4 @@
+import random
 import unittest
 
 import logging
@@ -7,6 +8,13 @@ import sys
 
 
 class KMeans(object):
+    """
+    Various schemes have been proposed for speeding up the K-means algorithm, some of which are based on precomputing a data structure such as a tree such that nearby points are in the same subtree (Ramasubramanian and Paliwal, 1990; Moore, 2000). Other approaches make use of the triangle inequality for distances, thereby avoiding unnecessary dis- tance calculations (Hodgson, 1998; Elkan, 2003).
+
+    But how to overcome Random initialization trap/local optima?
+    Use k-means++
+    """
+
     def __init__(self, k, log):
         """
         :param k: suppose for the moment that the value of K is given
@@ -16,10 +24,14 @@ class KMeans(object):
         self.debug = self.output.debug
         self.centers = []
 
+    def center_initialization(self, train_data):
+        self.debug('random initialization')
+        return train_data[np.random.choice(len(train_data), self.cluster_nums, replace=False)]
+
     def fit(self, train_data, iter_max=100):
         I = np.eye(self.cluster_nums)
         # self.debug(I)
-        centers = train_data[np.random.choice(len(train_data), self.cluster_nums, replace=False)]
+        centers = self.center_initialization(train_data)
 
         for i in range(iter_max):
             prev_centers = np.copy(centers)
@@ -57,6 +69,28 @@ class KMeans(object):
         return euclidean_dist
 
 
+class KMeansPP(KMeans):
+    """
+    KMeansPP means k-means++
+    it can eliminate the local optima by choosing the initial centers.
+    """
+
+    def center_initialization(self, train_data):
+        first_center = train_data[np.random.choice(len(train_data), 1, replace=False)]
+        centers = [first_center[0]]
+        while len(centers) < self.cluster_nums:
+            # np.power(np.sum(np.power(train_data[0] - centers[0], 2)), 1/2)
+            # (a, b) (c, d)  ((a-c)^2 + (b-d)^2)1/2
+            D2 = np.array([min([np.linalg.norm(x - c) ** 2 for c in centers]) for x in train_data])
+            cumprobs = (D2 / D2.sum()).cumsum()
+            print(D2.shape, train_data.shape, cumprobs.shape)
+            r = random.random()
+            chosen = train_data[cumprobs >= r][0]
+            centers.append(chosen)
+        print(centers)
+        return centers
+
+
 class TestKMeans(unittest.TestCase):
     def test_k_means(self):
         log = logging.getLogger("K-Means")
@@ -79,13 +113,26 @@ class TestKMeans(unittest.TestCase):
         plt.show()
 
 
-# class TestAlgorithm(unittest.TestCase):
-#     def runTest(self):
-#         log = logging.getLogger("TestAlgorithm")
-#         log.debug("hello world")
-
+class TestKmeanPP(unittest.TestCase):
+    def runTest(self):
+        log = logging.getLogger("TestKmeanPP")
+        x1 = np.random.normal(size=(100, 2))
+        x1 += np.array([-5, -5])
+        x2 = np.random.normal(size=(100, 2))
+        x2 += np.array([5, -5])
+        x3 = np.random.normal(size=(100, 2))
+        x3 += np.array([0, 5])
+        x_train = np.vstack((x1, x2, x3))
+        kpp = KMeansPP(3, log)
+        kpp.fit(x_train)
+        cluster = kpp.predict(x_train)
+        log.debug('->begin to draw gaussian models')
+        plt.scatter(x_train[:, 0], x_train[:, 1], c=cluster)
+        plt.scatter(kpp.centers[:, 0], kpp.centers[:, 1], s=200, marker='X', lw=2, c=['purple', 'cyan', 'yellow'],
+                    edgecolor="white")
+        plt.show()
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-    # unittest.TextTestRunner().run(TestAlgorithm())
-    unittest.main()
+    unittest.TextTestRunner().run(TestKmeanPP())
+    # unittest.main()
